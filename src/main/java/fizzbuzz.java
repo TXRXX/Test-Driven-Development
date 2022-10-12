0820d8b1dfce481ce21533834d11a92197f1d3da
savepunyapat <punyapat1412@gmail.com>
2022-10-12 15:13:03 +0700

    Dev test 1

---
+++ src/main/java/fizzbuzz.java
@@ -0,0 +1,9 @@
+public class fizzbuzz {
+    public String fz(int n){
+        if(n%3==0){
+            return "Fizz";
+        }else {
+            return ""+n;
+        }
+    }
+}
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,7 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class test {
     fizzbuzz f1;
     @Test
