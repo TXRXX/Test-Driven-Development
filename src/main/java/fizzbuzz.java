fe88fa9ae0767eab9956eccf87d845d675dc1726
savepunyapat <punyapat1412@gmail.com>
2022-10-12 15:27:05 +0700

    Dev test 3

--- src/main/java/fizzbuzz.java
+++ src/main/java/fizzbuzz.java
@@ -2,7 +2,10 @@ public class fizzbuzz {
     public String fz(int n){
         if(n%3==0){
             return "Fizz";
-        }else {
+        }else if(n%5==0){
+            return "Buzz";
+        }
+        else {
             return n+"";
         }
     }
