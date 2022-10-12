8664bce4a48b9c218244ae8bdabaa0b842a410ad
TXRXX <toro2544@gmail.com>
2022-10-12 15:24:43 +0700

    Testing #3 : Insert 5

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -11,12 +11,20 @@ public class test {
         String output = f1.fz(1);
         assertEquals("1",output);
     }
-    @Test
 
+    @Test
     @DisplayName("Insert 3")
     void test_FizzBuzz2() {
         f1 = new fizzbuzz();
         String output = f1.fz(3);
         assertEquals("3",output);
+    }
+
+    @Test
+    @DisplayName("Insert 5")
+    void test_FizzBuzz3() {
+        f1 = new fizzbuzz();
+        String output = f1.fz(5);
+        assertEquals("5",output);
     }
 }
