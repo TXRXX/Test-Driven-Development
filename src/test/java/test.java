9a494ef83fc34277cab94e06be11785034b0076c
TXRXX <toro2544@gmail.com>
2022-10-12 15:02:34 +0700

    Testing #1

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 public class test {
+    fizzbuzz f1;
+    @Test
+    @DisplayName("Insert 1")
+    void test_FizzBuzz() {
+        f1 = new fizzbuzz();
+        String output = f1.fz(1);
+        assertEquals("1",output);
+    }
 }
